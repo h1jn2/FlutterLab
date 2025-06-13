195291aa4d784b66f9433ca4d434cf17534120bb
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-06-13 10:40:53 +0900

    Text, Image

---
+++ lib/ch4/section2/test1.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  String longTxt = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리 나라 만세";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Image Text Test")),
+        body: Column(
+          children: [
+            Text(
+              "Hello World",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.red,
+                fontSize: 20,
+                backgroundColor: Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.wavy,
+              ),
+            ),
+            Text(
+              longTxt,
+              style: TextStyle(fontSize: 20),
+              maxLines: 2,
+              overflow: TextOverflow.fade,
+            ),
+            Image(
+              image: NetworkImage(
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Sunflower_from_Silesia2.jpg/1280px-Sunflower_from_Silesia2.jpg"
+              ),
+              width: 200,
+              height: 200,
+            ),
+            // 독자적인 내용을 출력하기 위한 위젯이 아니라
+            // 화면에 출력되는 특정 영역의 속성을 설정하기 위한 위젯
+            // 여백, 보더, 사이즈 등
+            Container(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: [
+                  Container(width: 200, height: 200, color: Colors.red),
+                  Image.asset(
+                    "images/big.jpeg",
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.none,
+                  ),
+                ],
+              ),
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("images/big.jpeg"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage("images/big.jpeg"),
+                      fit: BoxFit.cover
+                  )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
