cb6258fecef01ceac2cc92042682d707147180dc
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-06-13 13:39:13 +0900

    instagram-content

--- lib/ch4/section5/test1.dart
+++ lib/ch4/section5/test1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lab/ch4/section5/widgets/content_widget.dart';
 import 'package:flutter_lab/ch4/section5/widgets/header_widget.dart';
 import 'package:flutter_lab/ch4/section5/widgets/icon_widget.dart';
 import 'package:flutter_lab/ch4/section5/widgets/imgae_widget.dart';
@@ -8,15 +9,21 @@ main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // flutter 는 자동 스크롤을 지원하지 않음
+    // 동시에 여러 개의 위젯을 한번에 스크롤 -> ListView
+    // 단일 위젯을 스크롤 -> SingleChildScrollView
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("layout test"),),
-        body: Column(
-          children: [
-            HeaderWidget(),
-            ImageWidget(),
-            IconWidget()
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              HeaderWidget(),
+              ImageWidget(),
+              IconWidget(),
+              ContentWidget()
+            ],
+          ),
         ),
       ),
     );
